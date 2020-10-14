150684b6f2e3c305745f356b492bac0e194b88ea
John Collier <jcollier@redhat.com>
2020-10-14 13:55:42 -0400

    Update build.sh

--- index/generator/build.sh
+++ index/generator/build.sh
@@ -3,5 +3,4 @@ MODULE="github.com/devfile/registry-support/index/generator"
 BIN_DIR=$GOPATH/bin
 MODULE_BIN=$BIN_DIR/generator
 
-CGO_ENABLED=0 go install -mod=vendor $MODULE
-cp $MODULE_BIN ./index-generator
+CGO_ENABLED=0 go build -mod=vendor -o index-generator .
